9422a03c8b9be2b794bce484740422f2fb01287d
DannyGramowski <57734302+DannyGramowski@users.noreply.github.com>
2024-01-10 10:38:01 -0500

    fixed tool bar layouts

--- site/src/NavBar.js
+++ site/src/NavBar.js
@@ -1,24 +1,23 @@
 import { Component } from 'react';
 import { Container, Row, Col} from 'reactstrap';
-import { useNavigate } from "react-router-dom";
 
 import {Link} from "react-router-dom";
 export class NavBar extends Component {
     render() {
         return (
             <Container className='menu-bar'>
-                <Row className='menu-bar-item-row'>
-                    <Col>
-                        <Link to='/projects' className='menu-bar-item'>Projects</Link>
-                        {/* <p className='projects-redirect menu-bar-item' onClick={this.projectsRedirect}>Projects</p> */}
-                    </Col>
-                    <Col>
-                        <Link to='/resume' className='menu-bar-item'>Resume</Link>
-                    </Col>
-                    <Col>
-                        <Link to='/' className='menu-bar-item'>About</Link>
-                    </Col>
-                </Row>  
+                    <Row className='menu-bar-item-row '>
+                            <Col className='menu-bar-item-container'>
+                                <Link to='/projects' className='menu-bar-item'>Projects</Link>
+                                {/* <p className='projects-redirect menu-bar-item' onClick={this.projectsRedirect}>Projects</p> */}
+                            </Col>
+                            <Col className='menu-bar-item-container'>
+                                <Link to='/resume' className='menu-bar-item'>Resume</Link>
+                            </Col>
+                            <Col className='menu-bar-item-container'>
+                                <Link to='/' className='menu-bar-item'>About</Link>
+                            </Col>
+                    </Row>  
             </Container>
         )
     }
