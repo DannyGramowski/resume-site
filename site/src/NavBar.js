ed2b2071809c45dc9bb7abd15b28ed947f712125
DannyGramowski <57734302+DannyGramowski@users.noreply.github.com>
2024-01-10 22:18:49 -0500

    fixed navbar spacing

--- site/src/NavBar.js
+++ site/src/NavBar.js
@@ -15,27 +15,23 @@ export class NavBar extends Component {
         return (
             <Container className='menu-bar'>
                     <Row className='menu-bar-item-row '>
-                            {/* <Col className='menu-bar-item-container'>
-                                <Link to='/resume' className='menu-bar-item'>Resume</Link>
-                            </Col> */}
                             <Col className='menu-bar-item-container'>
                                 <Link to='/' className='menu-bar-item'>About</Link>
                             </Col>
                             <Col className='menu-bar-item-container'>
                                 <Link to='/projects' className='menu-bar-item'>Projects</Link>
-                                {/* <p className='projects-redirect menu-bar-item' onClick={this.projectsRedirect}>Projects</p> */}
                             </Col>
-                            <Col className='menu-bar-image-container'>
+                            <Col className='menu-bar-item-container'>
                                 <a href="https://dannytga.itch.io/" className='icon' target='_blank'>
                                     <img src={itch} width={width} height={itchRation * width}/>
                                 </a>
                             </Col>
-                            <Col className='menu-bar-image-container'>
+                            <Col className='menu-bar-item-container'>
                                 <a href="https://github.com/DannyGramowski" className='icon' target='_blank'>
                                     <img src={github} width={width} height={githubRatio * width}/>
                                 </a>
                             </Col>
-                            <Col className='menu-bar-image-container'>
+                            <Col className='menu-bar-item-container'>
                                 <a href="https://www.linkedin.com/in/daniel-gramowski-2b8391253/" className='icon' target='_blank'>
                                     <img src={linkedin} width={width} height={linkedinRation * width}/>
                                 </a>
