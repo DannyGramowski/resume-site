f12e15301f312efc61fd8c94bf7dc368f3e78e7d
DannyGramowski <57734302+DannyGramowski@users.noreply.github.com>
2026-02-16 11:30:19 -0500

    moved git ignore

--- site/src/Resume.js
+++ site/src/Resume.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import dannyResume from "./files/Danny_Resume.pdf";
+// import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 import './Resume.scss'
 
+
 // This approach imports the worker file directly from your installed package
+
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url,
+// ).toString();
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url,
 ).toString();
+
+// pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.mjs`;// var resumeURL = "";
+// if(dannyResume != "") {
+//     resumeURL = dannyResume;
+// } else {
+//     fetch(dannyResume).then((response) => {
+//         response.blob().then((blob) => {
+//             resumeURL = window.URL.createObjectURL(blob);
+//         });
+//     });
+// }
+// console.log(resumeURL, "resume url");
+console.log(dannyResume, "danny resume url");
 
 export const Resume = () => {
     return (
@@ -28,7 +48,7 @@ export const Resume = () => {
                 </Document>
             </div>
             
-            <a href={dannyResume} download className="custom-download-btn">
+            <a href={dannyResume} download="Daniel_Gramowski_Resume.pdf" className="custom-download-btn">
                 Download PDF
             </a>
         </div>
